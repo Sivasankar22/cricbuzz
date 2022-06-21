3d719d99930851d466e31769c06de6113319393f
Sivasankar22 <83053496+Sivasankar22@users.noreply.github.com>
2022-06-21 22:05:13 +0530

    Add files via upload

---
+++ src/cricbuzz/NewsFrame.java
@@ -0,0 +1,47 @@
+/* Suraj K
+ * 411731
+ */
+
+package cricbuzz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class NewsFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NewsFrame window = new NewsFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public NewsFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
